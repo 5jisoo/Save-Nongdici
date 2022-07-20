3e4baf7d4a2149647551117a25d8c78e2665275b
Jisoo Oh <ogisoo0606@gmail.com>
2022-07-20 20:43:13 +0900

    3_ChooseName 완료

--- save_nongdici/Assets/Scripts/ChooseName/SelectName.cs
+++ save_nongdici/Assets/Scripts/ChooseName/SelectName.cs
@@ -2,28 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SelectName : MonoBehaviour
 {
     public GameObject Player;
     public Text text;
+
+    public GameObject insertNamePlz;
+
+    public GameObject confirmName;
+    public Animator confirmNameAnim;
+    public Text confirmNameTxt;
+
     public PlayerData playerData;
-    public bool nameChecked = false;
+
+
 
     void Start()
     {
         playerData = Player.GetComponent<PlayerContoller>().playerData;
+        confirmNameAnim = confirmName.GetComponent<Animator>();
     }
 
     public void selectName()
     {
-        nameChecked = true;
-        if (nameChecked)
+        if (text.text != "")
         {
+            insertNamePlz.SetActive(false);
             playerData.name = text.text;
             Player.GetComponent<PlayerContoller>().SavePlayerDataToJson();
             print("저장완료");
+            confirmNameTxt.text = "'" + playerData.name + "'" + " (으)로";
+            confirmNameAnim.SetTrigger("SelectName");
         }
+        else
+        {
+            insertNamePlz.SetActive(true);
+        }
+    }
 
+    public void retryName()
+    {
+        SceneManager.LoadScene("3_ChooseName");
+    }
+
+    public void nameConfirm()
+    {
+        SceneManager.LoadScene("4_Stage1");
     }
 }
